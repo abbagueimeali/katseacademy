4d7d21fe843f9182bc3fa89092c1c8dc38e4dff7
Abba Gueime Ali <77186890+abbagueimeali@users.noreply.github.com>
2023-11-22 14:23:28 +0100

    Add files via upload

---
+++ src/main/java/com/logone/katseacademy/KatseacademyApplication.java
@@ -0,0 +1,13 @@
+package com.logone.katseacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatseacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatseacademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logone/katseacademy/KatseacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logone.katseacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatseacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
